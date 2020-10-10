43f38c0e9d4adc579b677e335236b36394d2e91e
Maryna Migacheva <m.migacheva@gmail.com>
2020-10-10 22:01:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let config=[];
+	let j = 0;
+	for(let i=0; i<bracketsConfig.length; i++) {
+		config[j] = bracketsConfig[i][0] + bracketsConfig[i][1];
+		j += 1;
+		}
+	let k = true;
+	while(k) {
+		for(let i=0; i<config.length; i++) {	
+			str = str.replace(config[i], '');	
+		};
+		k = false;
+		//проверяем, остались ли еще
+		for(let i=0; i<config.length; i++) {
+			if(str.indexOf(config[i]) !== -1) {k = true;}
+		}
+	}
+	if(str.length === 0) {return true} else return false;		
 }
